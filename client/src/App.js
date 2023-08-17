efbde30856484fdbfdb93b3798801e67da4fc8e7
intern_duyla <laianhduy04042001@gmail.com>
2023-08-17 11:20:49 +0700

    Create Project.

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App của Duy</h1>
+      <Button type="primary">Primary Button</Button>
+    </div>
+  );
+}
+
+export default App;
